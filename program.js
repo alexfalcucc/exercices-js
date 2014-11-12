de3273e98c9d53a82a7c67f6ca4301806b779ff7
alexfalcucc <alex.falcucci@gmail.com>
2014-11-12 19:43:17 +0000

    integration

--- program.js
+++ program.js
@@ -39,3 +39,70 @@ var fibonacci = function() {
 var array = ["zero", "one", "two"];
 
 console.log(array);
+
+document.writeln('\narray-> ' + array + "\ncumprimento-> " +  array.length);
+/*
+for (var i = 0; i < array; i += 1) {
+	document.writeln(array(i));
+}
+*/
+var data = [10,10,10];
+
+var add = function (a, b) {
+	result = a + b;
+	return result;
+}
+
+var mult = function (a, b) {
+	resultmul = a * b;
+	return resultmul;
+}
+
+var sum = data.reduce(add, 0);
+document.write("Array add numbers: " + sum);
+
+var mult = data.reduce(mult, 1);
+document.write("\nArray multiplied: " + mult)
+
+data.total = function () {
+	return this.reduce(add, 0);
+};
+
+total = data.total();
+document.write("\nTotal: " + total);
+
+// methods at array
+var matrix = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9]
+];
+
+Array.matrix = function (m, n, initial) {
+	var a, i, j, mat = [];
+	for (i = 0; i < m; i += 1) {
+		a = [];
+		for (j = 0; j < n; j +=1) {
+			a[j] = initial;
+		}
+		mat[i] = a;
+	}
+	return mat;
+};
+
+// Make a 4 * 4 matrix filled with zeros.
+var myMatrix = Array.matrix(4,4,0);
+document.writeln("\nElement at matrix: " + myMatrix[3][3]);
+
+// Method to make an identity matrix.
+Array.identity = function (n) {
+	var i, mat = Array.matrix(n, n, 0);
+	for (i = 0; i < n; i += 1) {
+		mat[i][i] = 1;
+	}
+	return mat;
+};
+
+myMatrix = Array.identity(4);
+
+document.writeln("\nMatrix identity: " + myMatrix[3][3]);
