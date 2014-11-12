2c1722ea7f60edaaab99702b35faffdd8f437192
alexfalcucci <alex.falcucci@hotmail.com>
2014-11-12 19:22:02 -0200

    regular expressions #1

--- program.js
+++ program.js
@@ -4,35 +4,35 @@
 var num = 10;
 
 var factorial = function factorial(i, a) {
-	a = a || 1;
-	if(i < 2) {
-		return a;
-	}
-	return factorial(i - 1, a * i);
+    a = a || 1;
+    if(i < 2) {
+        return a;
+    }
+    return factorial(i - 1, a * i);
 };
 
 
 //fibonacci version one
 var fibonacci = function (n) {
-	return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+    return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 for (var i = 0; i <= 10; i += 1) {
-	document.writeln("fibonacci de " + i + " é " + fibonacci(i));
+    document.writeln("fibonacci de " + i + " é " + fibonacci(i));
 }
 
 //fibonacci step two
 var fibonacci = function() {
-	var memo = [0, 1]
-	var fib = function (n) {
-		var result = memo[n];
-		if (typeof result !== 'number') {
-			result = fib(n - 1) + fib(n - 2);
-			memo[n] = result;			
-		}
-		return result;
-	};
-	return fib;
+    var memo = [0, 1]
+    var fib = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = fib(n - 1) + fib(n - 2);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return fib;
 }();
 
 
@@ -43,19 +43,19 @@ console.log(array);
 document.writeln('\narray-> ' + array + "\ncumprimento-> " +  array.length);
 /*
 for (var i = 0; i < array; i += 1) {
-	document.writeln(array(i));
+    document.writeln(array(i));
 }
 */
 var data = [10,10,10];
 
 var add = function (a, b) {
-	result = a + b;
-	return result;
+    result = a + b;
+    return result;
 }
 
 var mult = function (a, b) {
-	resultmul = a * b;
-	return resultmul;
+    resultmul = a * b;
+    return resultmul;
 }
 
 var sum = data.reduce(add, 0);
@@ -65,7 +65,7 @@ var mult = data.reduce(mult, 1);
 document.write("\nArray multiplied: " + mult)
 
 data.total = function () {
-	return this.reduce(add, 0);
+    return this.reduce(add, 0);
 };
 
 total = data.total();
@@ -73,21 +73,21 @@ document.write("\nTotal: " + total);
 
 // methods at array
 var matrix = [
-	[1,2,3],
-	[4,5,6],
-	[7,8,9]
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ];
 
 Array.matrix = function (m, n, initial) {
-	var a, i, j, mat = [];
-	for (i = 0; i < m; i += 1) {
-		a = [];
-		for (j = 0; j < n; j +=1) {
-			a[j] = initial;
-		}
-		mat[i] = a;
-	}
-	return mat;
+    var a, i, j, mat = [];
+    for (i = 0; i < m; i += 1) {
+        a = [];
+        for (j = 0; j < n; j +=1) {
+            a[j] = initial;
+        }
+        mat[i] = a;
+    }
+    return mat;
 };
 
 // Make a 4 * 4 matrix filled with zeros.
@@ -96,13 +96,47 @@ document.writeln("\nElement at matrix: " + myMatrix[3][3]);
 
 // Method to make an identity matrix.
 Array.identity = function (n) {
-	var i, mat = Array.matrix(n, n, 0);
-	for (i = 0; i < n; i += 1) {
-		mat[i][i] = 1;
-	}
-	return mat;
+    var i, mat = Array.matrix(n, n, 0);
+    for (i = 0; i < n; i += 1) {
+        mat[i][i] = 1;
+    }
+    return mat;
 };
 
 myMatrix = Array.identity(4);
 
 document.writeln("\nMatrix identity: " + myMatrix[3][3]);
+
+// Regular Expressions
+
+// Case of study
+
+var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+
+var url = "http://www.ora.com:80/goodparts?q#fragment";
+
+var result = parse_url.exec(url);
+
+var names = ['url', 'scheme', 'slash', 'host', 'port', 'path', 'query', 'hash'];
+
+var blanks = '      ';
+var i;
+
+for (i = 0; i < names.length; i += 1) {
+    document.writeln(names[i] + ':' + blanks.substring(names[i].length), result[i]);
+}
+
+var parse_number = /^-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?$/i;
+
+var test = function(num) {
+    document.writeln(parse_number.test(num));
+}
+
+document.writeln('<br>');
+
+test('1');
+test('number');
+test('98.6');
+test('132.21.86.100');
+test('123.45E-67');
+test('123.45D-67');
